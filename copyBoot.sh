654dd83f13756dfc162b2b62cf87a70886c092ee
Bryan Matthews <bmatthews@peninsulaconsulting.ca>
2020-02-18 14:07:28 -0800

    Updated hw-description to 2019.2

--- copyBoot.sh
+++ copyBoot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-petalinux-package --boot --force --fsbl images/linux/zynq_fsbl.elf --fpga zyboZ7Bd_wrapper.bit --u-boot
+petalinux-package --boot --force --fsbl images/linux/zynq_fsbl.elf --fpga project-spec/hw-description/zyboZ7.bit --u-boot
 
 cp images/linux/BOOT.BIN /media/dev/boot/
 cp images/linux/image.ub /media/dev/boot/
